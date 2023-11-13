13de7c331d8580e3676045f4f4da75b2c5fa1349
Zuhriddin1 <anarimon1985@gmail.com>
2023-11-13 18:15:52 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// let result = prompt("Resultat");
+// let a = result < 4 && a + b ? "Below" : "Over";
+// console.log(a);
+// let a = +prompt("Son kiriting");
+// let b = +prompt("Son kiriting");
+// if (a && b) {
+//   console.log("Teng sonlar");
+// } else {
+//   console.log("Bu teng emas");
+// }
+// let a = +prompt("Son kiriting");
+// let b = +prompt("Son kiriting");
+// if (a && b == 10) {
+//   alert("Uraaa jecpot");
+// } else {
+//   alert("No you don't win");
+// }
+// const score = prompt("Son kiriting");
+// if (score % 2 == 0) {
+//   console.log("Juft");
+// } else {
+//   console.log("Toq");
+// }
+// const score = prompt("1-9 kiriting");
+// alert("One two three four five six seven eight nine ten");
+//  const number = prompt("Sonni kiriting");
+// if (number == true || false) {
+//   alert("True");
+// } else {
+//   alert("False");
+// }
+// let a = +prompt("Son kiriting");
+// let b = +prompt("Son kiriting");
+// if (a == b) {
+//   alert("Teng");
+// } else {
+//   alert("Xato");
+// }
+// let a = +prompt("Tugilgan yillingiz qachon");
+// let b = +prompt("Tugilgan yillingiz qachon");
+// if (a == b) {
+//   alert("Sizlar tenglilar");
+// } else {
+//   alert("Sizlar xar xil yoshdasiz");
+// }
